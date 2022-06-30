378498365681211690fff04632350d09fc1f10f0
fatimakanthema <95094106+fatimakanthema@users.noreply.github.com>
2022-06-30 16:06:11 +0200

    Create var.java

---
+++ var.java
@@ -0,0 +1,13 @@
+public class main{
+  
+  public Static void main(String[]args){
+    
+    String firstName = "fatma";
+    String lastName = "kanthema";
+    String fullName = firstName + lastName;
+    
+    
+    System.out.println(fullName);
+  }
+  
+}
